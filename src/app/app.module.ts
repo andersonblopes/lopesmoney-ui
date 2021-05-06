45fc7e57842931fa427ec57e0ca18994ee6c25e3
Anderson Lopes <anderson.lopes.2010@gmail.com>
2021-05-07 01:29:24 +0200

    Checks primeng component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {TabViewModule} from 'primeng/tabview';
+
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, TabViewModule],
   providers: [],
   bootstrap: [AppComponent],
 })
